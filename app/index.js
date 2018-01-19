87a5e596db64772406c67e03f9faaee400810451
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-19 10:16:06 -0600

    Add dependencies and config

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main.jsx";
+
+const App = () => {
+  return <Main />;
+};
+ReactDOM.render(<App />, document.getElementById("app"));
